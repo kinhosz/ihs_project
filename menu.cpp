53acbd7361c8995dee02847e2bec7684fb925ff5
José Carlos <39632709+kinhosz@users.noreply.github.com>
2019-12-03 13:37:32 -0300

    FUNCIONANDO PORRAAAAAA

--- menu.cpp
+++ menu.cpp
@@ -1,11 +1,18 @@
 #include <bits/stdc++.h>
+#include <omp.h>
 using namespace std;
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
+#define barra_alt 13
+#define altura_define 555
+#define barra_larg 195
 #define FPS 60.0
 #define LARGURA_TELA 820
 #define ALTURA_TELA 580
 #define vel_barra 10
+#define total_threads 3
+#define AUMENTO_VELOCIDADE_BOLA 0.3
+typedef pair<int,int> ii;
 
 ALLEGRO_DISPLAY *janela = NULL;
 ALLEGRO_BITMAP *imagem = NULL;
@@ -30,18 +37,12 @@ struct Barra{
     int y;
 };
 
-struct Reta{ // para tratar colisao
-
-   double m;
-   double b;
-   double x;
-};
-vector<Reta> linha(4);
-
 vector<Block> campo;
 
 Ball bola;
 Barra barra;
+
+int score;
 
 void init(){
     al_init();
@@ -67,7 +68,7 @@ int flip_game(){
     al_draw_bitmap(imagem,0,0,0);
     al_draw_bitmap(barra_img,barra.x_in,barra.y,0);
     al_draw_bitmap(bola_img,bola.x,bola.y,0);
-
+    
     for(int i=0;i<campo.size();i++){
         if(campo[i].flag){
             al_draw_bitmap(bloco_img,campo[i].x,campo[i].y,0);
@@ -87,127 +88,180 @@ void registro_eventos(){
 }
 
 bool busca(){
-    //bola.vel+=0.3;
-    if(barra.x_in - 18 <= bola.x && barra.x_out >= bola.x + 18) return false;
-    return true;
+    bola.vel+=0.3;
+    if(barra.x_in - 18 <= bola.x && barra.x_out >= bola.x + 18) return true;
+    return false;
 }
 
-bool check(Block b){
+bool visit_all(){
+    int num_id;
+    int xa,xb,ya,yb;
+    bool crash;
+    int up_x= 0;
+    int down_x =0;
+    int up_y = 0;
+    int down_y = 0;
 
-    bool ret = false;
-    int ya,yb;
-    for(int i=0;i<4;i++){
-        ya = (linha[i].m * linha[i].x) + linha[i].b;
+    /*#pragma omp parallel num_threads(total_threads) private(num_id,xa,xb,ya,yb,crash)
+    {
+        crash = false;
+        num_id = omp_get_thread_num();
+        //fprintf(stderr,"thread %d\n",num_id);
+        fprintf(stderr,"posx -> %d, posy -> %d\n",bola.x,bola.y);
+        for(int i=num_id;i<112;i+=(112/total_threads)){
+            if(campo[i].flag){
+                xa = campo[i].x;
+                xb = campo[i].x + 45;
+                ya = campo[i].y;
+                yb = campo[i].y + 30;
+
+                if(bola.x >= xa && bola.x <= xb && bola.y >= ya && bola.y <= yb) crash = true;
+                else if(bola.x + 33 >= xa && bola.x + 33 <= xb && bola.y >= ya && bola.y <= yb) crash = true;
+                else if(bola.x + 33 >= xa && bola.x + 33 <= xb && bola.y + 33 >= ya && bola.y + 33 <= yb) crash = true;
+                else if(bola.x >= xa && bola.x <= xb && bola.y + 33 >= ya && bola.y + 33 <= yb) crash = true;
+
+                if(crash){
+                    
+                    #pragma omp atomic
+                    score++;
+
+                    campo[i].flag = false;
+                    if(bola.sent_x == 1){ // esquerda
+                        if(bola.x+33 == xa){
+                            #pragma omp atomic
+                            down_x++;
+                        }
+                    }
+                    else{ //direita
+                        if(bola.x == xb){
+                            #pragma omp atomic
+                            up_x++;
+                        }
+                    }
+
+                    if(bola.sent_y == 1){ // baixo
+                        if(bola.y + 33 == ya){ 
+                            #pragma omp atomic
+                            down_y++; 
+                        }
+                    }
+                    else{ // cima
+                        if(bola.y == yb){
+                            #pragma omp atomic
+                            up_y++;
+                        }
+                    }
+                }
+            }
+        }
+    }*/
+
+    for(int i=0;i<112;i++){
+        bool crash = false;
+        if(campo[i].flag){
+            xa = campo[i].x;
+            xb = campo[i].x + 45;
+            ya = campo[i].y;
+            yb = campo[i].y + 30;
+
+            if(bola.x >= xa && bola.x <= xb && bola.y >= ya && bola.y <= yb) crash = true;
+            else if(bola.x + 33 >= xa && bola.x + 33 <= xb && bola.y >= ya && bola.y <= yb) crash = true;
+            else if(bola.x + 33 >= xa && bola.x + 33 <= xb && bola.y + 33 >= ya && bola.y + 33 <= yb) crash = true;
+            else if(bola.x >= xa && bola.x <= xb && bola.y + 33 >= ya && bola.y + 33 <= yb) crash = true;
+
+            if(crash){
+                
+                ///#pragma omp atomic
+                score++;
+
+                campo[i].flag = false;
+                if(bola.sent_x == 1){ // esquerda
+                    if(bola.x+33 == xa){
+                        ///#pragma omp atomic
+                        down_x++;
+                    }
+                }
+                else{ //direita
+                    if(bola.x == xb){
+                        //// #pragma omp atomic
+                        up_x++;
+                    }
+                }
+
+                if(bola.sent_y == 1){ // baixo
+                    if(bola.y + 33 == ya){ 
+                        /// #pragma omp atomic
+                        down_y++; 
+                    }
+                }
+                else{ // cima
+                    if(bola.y == yb){
+                        /// #pragma omp atomic
+                        up_y++;
+                    }
+                }
+            }
+        }
     }
+
+    if(up_y) bola.sent_y = 1;
+    if(up_x) bola.sent_x = 1;
+    if(down_x) bola.sent_x = -1;
+    if(down_y) bola.sent_y = -1;
+
+    bool piso = false;
+
+    if(bola.x <= 0) bola.sent_x = 1;
+    else if(bola.x + 33 >= LARGURA_TELA) bola.sent_x = -1;
+
+    if(bola.y <= 0) bola.sent_y = 1;
+    else if(bola.y >= (altura_define-33)){ bola.sent_y = -1; piso = true;}
+    bool ret = true;
+    if(piso){
+       
+        bola.vel += AUMENTO_VELOCIDADE_BOLA;
+        if((bola.x >= barra.x_in && bola.x <= barra.x_out) || (bola.x + 33 >= barra.x_in && bola.x + 33 <= barra.x_out)){
+            bola.sent_y = -1;
+
+            if(bola.x <= barra.x_in + 40) bola.sent_x = -1;
+            else if(bola.x >= barra.x_out - 40) bola.sent_x = 1;
+            ret = true;
+        }
+        else ret = false;
+    }
+
+    return ret;
 }
-// calculando a reta da bola nos 4 pontos
- void reta(){
+int cont = 0;
+bool check_blocks(){
 
-    int x0,xf;
-    int y0,yf;
-    Reta aux;
-    // ponto inicial
-    x0 = bola.x;
-    y0 = bola.y;
-    xf = bola.x + (bola.sent_x * bola.vel);
-    yf = bola.y + (bola.sent_y * bola.vel);
+    int total = bola.vel;
+    bool ret = true;
+    bool aux;
 
-    double m;
-    m = (double)(yf - y0)/(xf - x0);
-    aux.m = m;
-    aux.b = y0;
-    aux.x = x0;
-    linha[0] = aux;
+    while(total > 0){
+        aux = visit_all();
+        if(!aux) ret = false;
+        if(bola.sent_x < 0) bola.x--;
+        else if(bola.sent_x > 0) bola.x++;
+        if(bola.sent_y < 0) bola.y--;
+        else if(bola.sent_y > 0) bola.y++;
+        total--;
+    }
 
-    // segundo ponto
-    x0 = bola.x + 33;
-    y0 = bola.y;
-    xf = (bola.x + 33) + (bola.sent_x * bola.vel);
-    yf = bola.y + (bola.sent_y * bola.vel);
-
-    m = (double)(yf - y0)/(xf - x0);
-    aux.m = m;
-    aux.b = y0;
-    aux.x = x0;
-    linha[1] = aux;
-
-    // terceiro ponto
-    x0 = bola.x;
-    y0 = bola.y + 33;
-    xf = bola.x + (bola.sent_x * bola.vel);
-    yf = (bola.y + 33) + (bola.sent_y * bola.vel);
-
-    m = (double)(yf - y0)/(xf - x0);
-    aux.m = m;
-    aux.b = y0;
-    aux.x = x0;
-    linha[2] = aux;
-
-    // quarto ponto
-    x0 = bola.x + 33;
-    y0 = bola.y + 33;
-    xf = (bola.x + 33) + (bola.sent_x * bola.vel);
-    yf = (bola.y + 33) + (bola.sent_y * bola.vel);
-
-    m = (double)(yf - y0)/(xf - x0);
-    aux.m = m;
-    aux.b = y0;
-    aux.x = x0;
-    linha[3] = aux;
+    return ret;
 }
 
-bool check_colision(){
+bool flip_pos_bola(){
 
-    int colidiu;
-    //reta();
-    for(int i=0;i<campo.size();i++){
-        //if(campo[i].flag) colidiu = check(campo[i]);
-        if(colidiu) break;
-    }
-
-    return colidiu;
-}
-
-bool flip_pos_bola(int dir){
-
-    bola.x += bola.sent_x * bola.vel;
-    bola.y += bola.sent_y * bola.vel;
-    int acres = 55;
-    int acres2 = 31;
-
-    bool resp = false;
-    int coef;
-    //add colisao aq
-    while(check_colision());
-    if(bola.y + acres >= ALTURA_TELA){
-        resp = busca();
-        bola.y = -acres + ALTURA_TELA - (bola.y + acres - ALTURA_TELA);
-        bola.sent_y *= -1;
-        if(dir == 1) bola.sent_x = -1;
-        else if(dir == 2) bola.sent_x = 1;
-    }
-    else if(bola.y < 0){
-        bola.y *= -1;
-        bola.sent_y *= -1;
-    }
-
-    if(bola.x + acres2 > LARGURA_TELA){
-        bola.x = -acres2 + LARGURA_TELA - (bola.x + acres2 - LARGURA_TELA);
-        bola.sent_x *= -1;
-    }
-    else if(bola.x < 0){
-        bola.x *= -1;
-        bola.sent_x *= -1;
-    }
-
-    return resp;
+    return check_blocks();
 }
 
 void restart_game(){
 
     barra.x_in = 340;
-    barra.x_out = 340+194;
-    barra.y = 555;
+    barra.x_out = 340+barra_larg;
+    barra.y = altura_define;
     bola.x = 400;
     bola.y = 350;
     bola.vel = 3;
@@ -252,6 +306,8 @@ int main(){
     int dir; // direcao que a barra esta indo, ajudara para identificar o movimento da bola
     while(!sair){
         //fprintf(stderr,"oi\n");
+        int last = -1;
+        score = 0;
         restart_game();
         flip_game();
         bool pressl = false;
@@ -272,7 +328,14 @@ int main(){
             int ret= 0;
             if(evento.type == ALLEGRO_EVENT_TIMER){
 
-                rodando = !flip_pos_bola(dir);
+                if(last != score){
+                    fprintf(stderr,"score is %d\n",score);
+                    last = score;
+                }
+
+                rodando = flip_pos_bola();
+                //rodando = check_colision();
+
                 dir = 0;
                 if(pressl){
                     dir = 1;
@@ -325,6 +388,8 @@ int main(){
                     break;
                 }
             }
+
+            cont++;
         }
 
         if(!sair){
